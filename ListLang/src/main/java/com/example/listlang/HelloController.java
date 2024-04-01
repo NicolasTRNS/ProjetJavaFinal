9302f557c8b53cf31a24868d3d0a085068757f9d
NicolasTRNS <157608348+NicolasTRNS@users.noreply.github.com>
2024-04-01 21:56:37 +0200

    Add files via upload

---
+++ ListLang/src/main/java/com/example/listlang/HelloController.java
@@ -0,0 +1,118 @@
+package com.example.listlang;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.*;
+
+public class HelloController implements Initializable {
+
+    @FXML
+    private ComboBox<String> taskSelected9;
+
+    @FXML
+    private Label PlantesVendues;
+
+    @FXML
+    private Button btnVendre;
+
+
+    @FXML
+    protected void vendrePlantes() {
+        // Code pour vendre les plantes
+
+        // Affichage du message
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Vente de plantes");
+        alert.setHeaderText("Les plantes ont été vendues !");
+        alert.setContentText(null); // Assurez-vous que le texte du contenu est null pour éviter de l'afficher à nouveau
+    }
+
+
+    @FXML
+    private ComboBox<String> taskSelected1;
+
+    @FXML
+    private ComboBox<String> taskSelected2;
+
+    @FXML
+    private ComboBox<String> taskSelected3;
+
+    @FXML
+    private ComboBox<String> taskSelected4;
+
+    @FXML
+    private TabPane myTab;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialiser les ComboBox avec les données nécessaires
+        taskSelected1.getItems().addAll("Sofia Ramirez", "Xavier Johnson", "Jasmine Patel");
+        taskSelected2.getItems().addAll("sofia.ramirez@gmail.com", "xavier.johnson@gmail.com", "jasmine.pastel@gmail.com");
+        taskSelected3.getItems().addAll("Géranium", "Rosier", "Cactus");
+        taskSelected4.getItems().addAll("1", "2", "3", "4", "5");
+        taskSelected9.getItems().addAll("Géranium", "Rosier", "Cactus");
+
+        // Sélection aléatoire des valeurs
+        Random random = new Random();
+        int selectedIndex = random.nextInt(taskSelected1.getItems().size());
+        taskSelected1.getSelectionModel().select(selectedIndex);
+        taskSelected2.getSelectionModel().select(selectedIndex); // Sélection basée sur le même index que taskSelected1
+
+        taskSelected3.getSelectionModel().select(random.nextInt(taskSelected3.getItems().size()));
+        taskSelected4.getSelectionModel().select(random.nextInt(taskSelected4.getItems().size()));
+
+        // Assurez-vous que taskSelected9 sélectionne un élément différent de taskSelected3
+        int selectedIndex9;
+        do {
+            selectedIndex9 = random.nextInt(taskSelected9.getItems().size());
+        } while (selectedIndex9 == selectedIndex);
+        taskSelected9.getSelectionModel().select(selectedIndex9);
+
+        // Liaison de l'action du bouton btnVendre avec l'actualisation de PlantesVendues
+        btnVendre.setOnAction(event -> PlantesVendues.setText("New label text"));
+    }
+
+
+    @FXML
+    private void passerAVente() {
+        myTab.getSelectionModel().select(2); // Sélectionne l'onglet "Vente"
+    }
+
+    @FXML
+    protected void quitter() {
+        // Méthode pour quitter l'application
+        Platform.exit(); // Cette ligne permet de fermer l'application
+    }
+
+
+
+    @FXML
+    protected void changeToModify() {
+        // Méthode pour charger les données à modifier
+    }
+
+
+
+    @FXML
+    protected void changeToList() {
+        // Méthode pour charger la liste des tâches
+    }
+
+    @FXML
+    protected void selectedTask() {
+        // Méthode pour afficher les détails d'une tâche sélectionnée
+    }
+
+
+
+    @FXML
+    protected void verifierInventaire() {
+        // Méthode pour rediriger vers l'onglet "Inventaire" lorsque le bouton "Vérifier l'inventaire" est cliqué
+        myTab.getSelectionModel().select(1); // Sélectionne l'onglet "Inventaire"
+    }
+}
---
+++ ListLang/src/main/java/com/example/listlang/LoginController.java
@@ -0,0 +1,124 @@
+package com.example.listlang;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javafx.scene.Parent;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label loginMessage;
+
+    // Méthode appelée lorsqu'on clique sur le bouton de connexion
+    @FXML
+    private void handleLoginAction(ActionEvent event) {
+        usernameField.getText();
+        passwordField.getText();
+
+        // Vérifier si l'utilisateur existe dans la base de données
+        if (checkUserExists()) {
+            loginMessage.setText("Connexion réussie !");
+            redirectToHelloView(event);
+        } else {
+            loginMessage.setText("Identifiants incorrects. Veuillez réessayer.");
+        }
+    }
+
+    // Méthode appelée lorsqu'on clique sur le bouton S'inscrire
+    @FXML
+    private void handleInscriptionAction(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Vérifier si les champs ne sont pas vides
+        if (username.isEmpty() || password.isEmpty()) {
+            loginMessage.setText("Veuillez remplir tous les champs.");
+        } else {
+            // Vérifier si l'utilisateur existe déjà dans la base de données
+            if (checkUserExists()) {
+                loginMessage.setText("L'utilisateur existe déjà !");
+            } else {
+                // Enregistrer le nouvel utilisateur dans la base de données
+                registerNewUser(username, password);
+                loginMessage.setText("Inscription réussie !");
+                redirectToHelloView(event);
+            }
+        }
+    }
+
+    // Vérifier si l'utilisateur existe dans la base de données
+    private boolean checkUserExists() {
+        // Votre logique pour vérifier si l'utilisateur existe va ici
+        return false;
+    }
+
+    // Ajouter un nouvel utilisateur à la base de données
+    private void registerNewUser(String username, String password) {
+        // Connexion à la base de données
+        String url = "jdbc:mysql://localhost:3306/projet_java_db";
+        String user = "root";
+        String dbPassword = "";
+
+        try (Connection connection = DriverManager.getConnection(url, user, dbPassword)) {
+            // Requête SQL pour insérer un nouvel utilisateur dans la table 'users'
+            String query = "INSERT INTO users (username, password) VALUES (?, ?)";
+
+            // Préparation de la requête
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            // Exécution de la requête
+            statement.executeUpdate();
+
+            // Fermeture des ressources
+            statement.close();
+        } catch (SQLException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+
+    // Redirection vers l'application principale (hello-view.fxml)
+    private void redirectToHelloView(ActionEvent event) {
+        try {
+            // Charger le fichier FXML hello-view.fxml
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Créer une nouvelle scène avec le contenu du fichier FXML chargé
+            Scene scene = new Scene(root);
+
+            // Obtenir la référence de la fenêtre actuelle à partir de l'événement
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Définir la nouvelle scène sur la fenêtre
+            stage.setScene(scene);
+            stage.setTitle("JardinoTech"); // Titre de la nouvelle fenêtre
+
+            // Afficher la fenêtre
+            stage.show();
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+}
